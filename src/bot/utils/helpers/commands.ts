32f7384f68266e1848cfebdded09d9a69028f73b
Bedo <bedonassef71@gmail.com>
2024-04-14 10:53:49 +0200

    handle error

--- src/bot/utils/helpers/commands.ts
+++ src/bot/utils/helpers/commands.ts
@@ -2,6 +2,6 @@ import { Command } from '../types/command.type';
 
 export const commands: Command[] = [
   { command: 'start', description: 'Show list of available services.' },
-  { command: 'help', description: 'Learn more about how to use this bot.' },
+  // { command: 'help', description: 'Learn more about how to use this bot.' },
   { command: 'info', description: 'Learn more about me and what I do.' },
 ];
--- src/weather/handle-weather-option.ts
+++ src/weather/handle-weather-option.ts
@@ -4,14 +4,19 @@ import { bot } from '../bot';
 
 // Function to handle weather option
 export async function handleWeatherOption(chatId: ChatId): Promise<void> {
-  const states = await getAllCities();
-  const keyboard = states.map((state: any) => [
-    { text: state.name, callback_data: `weather_${state.id}_${state.name}` },
-  ]);
+  try {
+    const states = await getAllCities();
+    const keyboard = states.map((state: any) => [
+      { text: state.name, callback_data: `weather_${state.id}_${state.name}` },
+    ]);
 
-  bot.sendMessage(chatId, 'Select a state:', {
-    reply_markup: {
-      inline_keyboard: keyboard,
-    },
-  });
+    bot.sendMessage(chatId, 'Select a state:', {
+      reply_markup: {
+        inline_keyboard: keyboard,
+      },
+    });
+  } catch (error) {
+    console.error('Error handling weather option:', error);
+    bot.sendMessage(chatId, 'An error occurred while fetching weather information. Please try again later.');
+  }
 }
